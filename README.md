ce550d969a1d82c337aeca5452869c136e1de2ab
smarcam1108 <smarcam1108@g.educaand.es>
2025-04-08 10:10:11 +0200

    Nueva info

---
+++ README.md
@@ -0,0 +1,20 @@
+# Ordenación. Algoritmos básicos
+Repositorio para profundizar en python estudiando cómo ordenar una lista
+Autor: Yo mismo
+Fecha: Hoy
+Organización: IES José de Mora
+
+# Algoritmos:
+Para cambiar de orden dos datos se una una variable *auxiliar*, de modo que 
+almacenamos el primer dato de la lista en ella, y luego el segundo dato en el espacio
+del primero, para terminar volcando la variable auxiliar en el segundo dato. Esto se
+conoce como *intercambio* o *swap*.
+
+Para tres datos hay que hacer ```swap()´´´ con el primer y segundo dato, después con 
+el segundo y tercero, y finalmente otra vez con el primero y segundo
+
+## Otros aprendizajes
+
+1.Introducir datos con el terminal
+2.Documentar los proyectos
+3.Manejar una GUI básica
--- dos_valores.py
+++ dos_valores.py
@@ -1,9 +1,42 @@
-# Se trata de tomar una lista de los valores desordenados 
-# y producir una salida en orden creciente.
+# Archivo: tres_valores.property.py 
+# Fecha: 08/04/25
+# Proy.: estudio de algoritmos
+#
+# Ordenar crecientemente una lista de tres valores
+# Existe un error, ya que según los valores, puede cambiar
+# dos que entre ellos había que cambiar, pero genera mal orden
+# en los posteriores
 
-n = [15, 23]
-if n[0] > n[1]:
-    a = n[0]
-    n[0] = n[1]
-    n[1] = a
-print (n)
+# Función swap (int indice)
+# cambia de orden dos elementos de una lista ya existente s
+# 
+def swap(indice):
+    a = s[indice]
+    s[indice] = s[indice]
+    s[indice] = a 
+
+
+n = []
+
+# Bucle para tomar datos del teclado y guardarlos en la variable n de tipo lista
+for i in range(3):
+    n.append(int(input())) # append es un método de las listas
+
+s = n # Voy a hacer dos intentos de ordenar. Para tener 
+      # la lista original sin cambios, tendré que copiarla a otra lista
+      # y así trabajamos con la copia
+
+for i in range(5):   # Aquí se refelja el error p.ej. con
+    if s[i] > s[i+1]:
+        a = s[i]
+        s[i] = s[i+1]
+        s[i+1] = a
+print(s)
+
+s = n
+for i in range(len(n)-2):     #Aquí hacemos el repaso a la lista dos veces 
+    for j in range (len(n)-1):
+        if s[j] > s[j+1]:
+            swap(j)
+print(s)
+
